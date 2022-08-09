17daf208d7d152a89ddfbfc5995a1bcf89dc8f26
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2022-08-09 20:15:49 +0200

    First commit to GitHub

---
+++ read_nc.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  9 18:26:51 2022
+
+@author: lasse
+"""
+#Importing necessary libraries
+from scipy import io
+import pandas as pd
+import os
+
+print("Choose file to extract XYZ data from: ")
+
+#Retrieving absolute path of this file and setting it to current directory.
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+#Setting input filepath
+nc_filepath = os.path.join(os.getcwd()+'/nc_files')
+# Setting output filepath
+xyz_filepath = os.path.join(os.getcwd()+'/xyz_files')
+
+#Creating dict of filenames in input path.
+fileList_dict = {str(idx):file for idx,file in enumerate(os.listdir(nc_filepath))}
+
+for idx in fileList_dict:
+    #Printing each filename in the filepath
+    print('[%s] %s' % (idx, fileList_dict[idx]))
+
+print("")
+print('[%d] All' % (len(fileList_dict)))
+
+#User chooses which of the files to retrieve XYZ from. All is an option.
+choice = input("Choose option: ")
+
+#Setting convert_all depending on user choice
+if choice == len(fileList_dict):
+    convert_all = 1
+
+#Creating folder for converted files
+if not os.path.isdir(dname+'/xyz_files'):
+    os.mkdir(os.path.join(dname+r'\xyz_files'))
+    print("Created folder for xyz files (%s)" % os.path.join(dname+r'\xyz_files'))
+
+
+def sameNumVariables(x, y, z):
+    """    Checks if X, Y and Z are of equal length     """
+    if len(x) == len(y) and len(x) == len(z):
+        return 1
+    else:
+        return 0
+
+
+
+def extract_xyz(filepath):
+    print("[INFO] LOADING FIRST FILE ...")
+    
+    data = io.netcdf_file(filepath, mode='r')
+    #Retrieving filename without .filetype extension.
+    filename = os.path.basename(filepath).split('.')[0]
+    print("[INFO] FILE: %s" % filename)
+    
+    #setting default keys for x, y and z
+    x_key = 'lat'
+    y_key = 'lon'
+    z_key = 'lat'
+    
+    #Creating while loop that runs until a valid key is chosen for x, y and z data.
+    
+    invalidKey = 1
+    while invalidKey:
+        try:
+            x = data.variables[x_key][:].copy()
+            invalidKey = 0
+        except KeyError:
+            print("")
+            print("[ERROR] %s not a variable in the data..." % x_key)
+            print("[INFO] List of variables: ")
+            # Prompts user to type in a desired key for the X-data
+            print([variable for variable in data.variables.keys()])
+            x_key = input("Choose new X var: ")
+            
+    invalidKey = 1    
+    while invalidKey:
+        try:
+            y = data.variables[y_key][:].copy()
+            invalidKey = 0
+        except KeyError:
+            print("")
+            print("[ERROR] %s not a variable in the data..." % y_key)
+            print("[INFO] List of variables: ")
+            # Prints list of valid variable keys to choose
+            print([variable for variable in data.variables.keys()])
+            # Prompts user to type in a desired key for the Y-data
+            y_key = input("Choose new Y var: ")   
+    
+    invalidKey = 1
+    while invalidKey:
+        try:
+            z = data.variables[z_key][:].copy()
+            invalidKey = 0
+        except KeyError:
+            print("")
+            print("[ERROR] %s not a variable in the data..." % z_key)
+            print("[INFO] List of variables: ")
+            # Prints list of valid variable keys to choose
+            print([variable for variable in data.variables.keys()])
+            # Prompts user to type in a desired key for the Z-data
+            z_key = input("Choose new Z var: ")
+    
+
+    #Checking if X, Y and Z are of equal length
+    varListEqualLength = sameNumVariables(x, y, z)
+    if varListEqualLength:
+        #Creating dataframe
+        df = pd.DataFrame(list(zip(x, y, z)), columns=[x_key, y_key, z_key])
+        #Exporting values to csv file
+        df.to_csv(xyz_filepath + '/' + filename + '_xyz.csv', index=False)  
+        print("====================================")
+        print("[SUCCES] %s_xyz.csv printed to %s" % (filename, xyz_filepath))
+        print("====================================")
+    else:
+        print("[ERROR] List of variables for export are not equal length ... Values were not exported")  
+        print("   X: %d values" % len(x))  
+        print("   Y: %d values" % len(y))  
+        print("   Z: %d values" % len(z))
+        
+if int(choice) < len(fileList_dict):
+    extract_xyz(os.path.join('%s/%s' % (nc_filepath, fileList_dict[choice]) 
+                              ) 
+                )
+elif int(choice) >= len(fileList_dict):
+    [extract_xyz(os.path.join('%s/%s' % (nc_filepath, fileList_dict[str(idx)]))) for idx in range(0, len(fileList_dict))]
+
+    
---
+++ readme.txt
@@ -0,0 +1,34 @@
+Readme for read_nc.py
+
+
+### Requires libraries pandas and scipy ###
+
+## Place read_nc.py in a directory containing a folder named "nc_files" (see tree below)
+
+Directory:
+│   readme.txt
+│   read_nc.py
+│
+├───nc_files
+│       cami_0000-09-01_64x128_L26_c030918.nc
+│       ECMWF_ERA-40_subset.nc
+│       madis-hydro.nc
+│       madis-mesonet.nc
+│       madis-metar.nc
+│       madis-raob.nc
+│       madis-sao.nc
+│       tos_O1_2001-2002.nc
+│
+└───xyz_files
+        madis-hydro_xyz.csv
+
+
+Processed files are written to /xyz_files/
+
+## change dir
+cd /d path/to/read_nc.py
+
+## run script
+python read_nc.py
+
+From here follow instructions...
