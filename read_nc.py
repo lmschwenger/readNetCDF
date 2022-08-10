78d68bc09dc5b9b899569243eaee6e663adaa346
Lasse Schwenger <lasse.m.schwenger@gmail.com>
2022-08-11 01:04:09 +0200

    Code cleaning and constraint on choosing file number not listed

--- read_nc.py
+++ read_nc.py
@@ -9,8 +9,6 @@ import netCDF4 as nc
 import pandas as pd
 import os
 
-
-
 #Retrieving absolute path of this file and setting it to current directory.
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -23,23 +21,20 @@ xyz_filepath = os.path.join(os.getcwd()+'/xyz_files')
 #Creating dict of filenames in input path.
 fileList_dict = {str(idx):file for idx,file in enumerate(os.listdir(nc_filepath))}
 
-print("[PROMPT] Choose file to extract XYZ data from: ")
-for idx in fileList_dict:
-    #Printing each filename in the filepath
-    print('[%s] %s' % (idx, fileList_dict[idx]))
-    
+""" Shows list of files in /nc_files/ that can be converted """
+
+print("[INFO] FILES FOUND IN /nc_files/: ")
+[print('[%s] %s' % (i, fileList_dict[i])) for i in fileList_dict.keys()] #Printing files
 print("")
 print('[%d] All' % (len(fileList_dict)))
-
 #User chooses which of the files to retrieve XYZ from. All is an option.
-choice = input("[PROMPT] Choose option: ")
+choice = input("[PROMPT] Choose file to extract XYZ data from: ")
 
 
 #Creating folder for converted files
 if not os.path.isdir(dname+'/xyz_files'):
     os.mkdir(os.path.join(dname+r'\xyz_files'))
     print("Created folder for xyz files (%s)" % os.path.join(dname+r'\xyz_files'))
-
 
 def sameNumVariables(x, y, z):
     """    Checks if X, Y and Z are of equal length     """
@@ -48,34 +43,37 @@ def sameNumVariables(x, y, z):
     else:
         return 0
 
-
+def invalidKeyMsg(key):
+    """  
+        Returns an error message to cmd if specified 
+        key is not a variable in the dataset 
+                                                    """
+    print("")
+    print("[ERROR] %s not a variable in the data..." % key)
+    print("[INFO] List of variables: ")
+    
 
 def extract_xyz(filepath):
     
-    print("[INFO] LOADING FIRST FILE ...")
+    print("[INFO] LOADING FILE ...")
     data = nc.Dataset(filepath, mode='r')
-    
     #Retrieving filename without .filetype extension.
     filename = os.path.basename(filepath).split('.')[0]
     print("[INFO] FILE: %s" % filename)
-    
     #setting default keys for x, y and z
     x_key = 'latitude'
     y_key = 'longitude'
     z_key = 'latitude'
     
     #Creating while loop that runs until a valid key is chosen for x, y and z data.
-    
     invalidKey = 1
     while invalidKey:
         try:
             x = data.variables[x_key][:].copy()
             invalidKey = 0
         except KeyError:
-            print("")
-            print("[ERROR] %s not a variable in the data..." % x_key)
-            print("[INFO] List of variables: ")
-            
+            #Returns cmd message stating key (variable) is not in dataset
+            invalidKeyMsg(x_key)
             # Prompts user to type in a desired key for the X-data
             print([variable for variable in data.variables.keys()])
             x_key = input("[PROMPT] Choose new X var: ")
@@ -86,13 +84,10 @@ def extract_xyz(filepath):
             y = data.variables[y_key][:].copy()
             invalidKey = 0
         except KeyError:
-            print("")
-            print("[ERROR] %s not a variable in the data..." % y_key)
-            print("[INFO] List of variables: ")
-            
+            #Returns cmd message stating key (variable) is not in dataset
+            invalidKeyMsg(y_key)
             # Prints list of valid variable keys to choose
             print([variable for variable in data.variables.keys()])
-            
             # Prompts user to type in a desired key for the Y-data
             y_key = input("[PROMPT] Choose new Y var: ")   
     
@@ -101,28 +96,24 @@ def extract_xyz(filepath):
         try:
             z = data.variables[z_key][:].copy()
             invalidKey = 0
-            
         except KeyError:
-            print("")
-            print("[ERROR] %s not a variable in the data..." % z_key)
-            print("[INFO] List of variables: ")
-            
+            #Returns cmd message stating key (variable) is not in dataset
+            invalidKeyMsg(z_key)
             # Prints list of valid variable keys to choose
             print([variable for variable in data.variables.keys()])
-            
             # Prompts user to type in a desired key for the Z-data
             z_key = input("[PROMPT] Choose new Z var: ")
     
     
+
     #Checking if X, Y and Z are of equal length
     varListEqualLength = sameNumVariables(x, y, z)
     if varListEqualLength:
-
+        """ If all variables have same number of values --> Start compiling export files """
         #Creating dataframe
         df = pd.DataFrame(list(zip(x, y, z)), columns=[x_key, y_key, z_key])
         #Exporting values to csv file
         df.to_csv(xyz_filepath + '/' + filename + '_xyz.csv', index=False)
-        
         #Creating .txt file with units of the exported data.
         with open(xyz_filepath + '/' + filename + '_units.txt', 'w') as writer:
             writer.write('Variable,Unit\n')   
@@ -134,7 +125,6 @@ def extract_xyz(filepath):
         print("[SUCCES] %s_xyz.csv printed to %s" % (filename, xyz_filepath))
         print("[SUCCES] %s_units.csv printed to %s" % (filename, xyz_filepath))
         print("====================================")
-        
     else:
         print("[ERROR] List of variables for export are not equal length ... Values were not exported")  
         print("   X: %d values" % len(x))  
@@ -144,11 +134,12 @@ def extract_xyz(filepath):
     data.close()        
 
 # Perform extraction task based on file-prompt.
-if int(choice) < len(fileList_dict):
+if int(choice) < len(fileList_dict) and int(choice) >= 0:
     extract_xyz(os.path.join('%s/%s' % (nc_filepath, fileList_dict[choice]) 
                               ) 
                 )
 elif int(choice) >= len(fileList_dict): #Performs extraction on all files in nc_files/
     [extract_xyz(os.path.join('%s/%s' % (nc_filepath, fileList_dict[str(idx)]))) for idx in range(0, len(fileList_dict))]
 
-    
+else:
+    print("Invalid index for file. Try over and only choose values between 0 and %s" % len(fileList_dict))
